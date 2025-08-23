71fb44c2669247daa651597a6de4b3a0f5b49293
Warren Mo <wmowkm@gmail.com>
2025-08-23 14:32:34 -0400

    leetgpu/count_2d_array_element/pytorch.py

---
+++ leetgpu/count_2d_array_element/pytorch.py
@@ -0,0 +1,5 @@
+import torch
+
+# input, output are tensors on the GPU
+def solve(input: torch.Tensor, output: torch.Tensor, N: int, M: int, K: int):
+    output.copy_(torch.count_nonzero(input == K))
