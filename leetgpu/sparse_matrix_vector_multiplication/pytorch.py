28a87655fca7e6e12bd941cccab4de1f2e20bb1e
Warren Mo <wmowkm@gmail.com>
2025-08-24 17:25:19 -0400

    typo fix

--- leetgpu/sparse_matrix_vector_multiplication/pytorch.py
+++ leetgpu/sparse_matrix_vector_multiplication/pytorch.py
@@ -3,7 +3,7 @@ import torch
 # A, x, y are tensors on the GPU
 def solve(A: torch.Tensor, x: torch.Tensor, y: torch.Tensor, M: int, N: int, nnz: int):
     """
-    It makes more mathetmatical sense (to me) that we'd need to write `x1 as
+    It makes more mathematical sense (to me) that we'd need to write `x1 as
     `x.view(N, 1)` for "proper" matrix multiplication. But if you do, then
     you'll have to call `y.squeeze_()`, because `y` will be of shape (N,1)
     rather than (1,N), which is what we want.
